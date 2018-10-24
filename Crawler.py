0fb3176c3535797788eaae004b136309b60053a5
Farhad Mortazapour <farhadmp@gmail.com>
2018-10-24 12:01:13 +0330

    Add new features to crawler

--- Crawler.py
+++ Crawler.py
@@ -1,9 +1,7 @@
 def get_next_target(content):
     start_link = content.find('<a href=')
-
     if start_link==-1:
         return None, 0
-
     start_quote = content.find('"', start_link)
     end_quote = content.find('"', start_quote+1)
     url = content[start_quote+1:end_quote]
@@ -20,10 +18,48 @@ def print_all_links(content):
             break
 
 
+def get_all_links(content):
+    """
+    Takes the content of a web page and
+    returns all of its links in a list.
+    """
+    links=[]
+    while True:
+        url, end_quote = get_next_target(content)
+        if url:
+            links.append(url)
+            content = content[end_quote:]
+        else:
+            break
+    return links
+
+
 def get_page_content(url):
     import urllib.request
-    return urllib.request.urlopen(url).read().decode('utf-8')
+    try:
+        req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})   
+        res = urllib.request.urlopen(req).read().decode('utf-8')
+    except: 
+        res=""
+    return res
 
 
-seed = 'http://w3schools.com'
-print_all_links(get_page_content(seed))
+def crawl(seed="https://www.wikipedia.org"):    
+    """
+    Crawl web using Breadth-First-Search
+    """    
+    to_crawl = [seed]
+    crawled = []
+    while to_crawl:
+        url = to_crawl.pop(0)
+        if url not in crawled:
+            links = get_all_links(get_page_content(url))
+            #union(to_crawl, links)
+            to_crawl=list(set().union(to_crawl, links))
+            crawled.append(url)
+            print(url)
+            if len(crawled) == 3000: break
+    return crawled
+
+crawl()
+
